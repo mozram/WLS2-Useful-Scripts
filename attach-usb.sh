3d23944a460aed01ad9cadfb8cb2eb888e9cae84
Mohd Zulhilmi Ramli <mohdzulhilmi.ramli@dyson.com>
2023-08-28 09:47:10 +0800

    Add attach usb device to WSL2

---
+++ attach-usb.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+## Must set an argument of the USB ID to attach. Use lsusb to get the ID.
+## For example attach-usb.sh 1050:0407
+
+set -e
+
+USBIPD_BIN="usbipd.exe"
+
+# Check args
+if [ -z "${1}" ]; then
+    echo "No device specified. Exitting..."
+    exit
+else 
+    USB_ID=${1}
+    echo "Attaching $USB_ID"
+fi
+
+# Get busid based on device ID
+USB_BUSID=$($USBIPD_BIN wsl list | grep "$USB_ID" | awk '{print $1}')
+
+# Attach the device, and set auto attach
+$USBIPD_BIN wsl attach -b $USB_BUSID -a
